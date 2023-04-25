8c5b63e91dabcca59141f0ff92c8c15814412a37
joaors10 <joaosolano04@gmail.com>
2023-04-25 11:43:21 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,39 @@
+//buscando a lista de um array usando for
+
+let msg=document.getElementById('msg')
+
+const usuarios=[
+    {nome: "Huguinho", idade:18},
+    {nome:"Zezinho", idade:19},
+    {nome:"Luizinho", idade:20},
+];
+
+
+const Users = usuarios.length;
+
+for (let i=0; i<Users; i++){
+    msg.innerHTML += `<p>${usuarios[i].nome} ${usuarios[i].idade}</p>`
+}
+
+
+var imagens=[
+    'img/1.jpg',
+    'img/2.jpg', 
+    'img/3.jpg',
+];
+
+/*declarando variavel*/
+var Index =0;
+var time =2000;
+
+/*criando uma função*/
+function slideShow(){
+    document.getElementById('image').src= imagens[Index];
+    Index++;
+    if (Index == imagens.length) (Index =0);
+    setTimeout("slideShow()", time);
+}
+
+slideShow();
+
+
